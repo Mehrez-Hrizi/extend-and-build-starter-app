22760fae9d7e9d1c3d7141a10c2e9a92e5b0fbbe
Mehrez Hrizi <mehrez.hrizi.official@gmail.com>
2022-04-22 11:48:13 +0100

    add custom view + create the updated routing array

--- src/bundles/accounts-transactions-journey-bundle.module.ts
+++ src/bundles/accounts-transactions-journey-bundle.module.ts
@@ -2,17 +2,48 @@ import { NgModule } from '@angular/core';
 import { 
     AccountsTransactionsJourneyConfiguration,
     AccountsTransactionsJourneyConfigurationToken,
-    AccountsTransactionsJourneyModule 
+    AccountsTransactionsJourneyModule,
+    AccountsTransactionsJourneyComponent,
+    AccountsListComponent,
+    AccountsManageGuardService 
 } from '@backbase/accounts-transactions-journey-ang'
+
+import { Route } from '@angular/router';
+
+import { CustomAccountsManagementWidgetModule } from 'src/app/custom-views/accounts/custom-accounts-management.module';
+import { CustomAccountsManageComponent } from 'src/app/custom-views/accounts/custom-accounts-management.component';
  
+const customRoutes: Route = {
+    path: '',
+    component: AccountsTransactionsJourneyComponent,
+    children: [
+    {
+        path: '',
+        redirectTo: 'list',
+        pathMatch: 'full',
+    },
+    {
+        path: 'list',
+        component: AccountsListComponent,
+        data: { title: 'My Accounts' },
+    },
+    {
+        path: 'manage',
+        component: CustomAccountsManageComponent, //<-- NEW CUSTOM VIEW
+        canActivate: [AccountsManageGuardService],
+    }
+    ],
+}; 
+
 @NgModule({
-    imports: [AccountsTransactionsJourneyModule.forRoot()],
+    imports: [AccountsTransactionsJourneyModule.forRoot({route:customRoutes}),
+    CustomAccountsManagementWidgetModule],
     providers: [{
         provide: AccountsTransactionsJourneyConfigurationToken,
         useValue: <Partial<AccountsTransactionsJourneyConfiguration>>{
           showCheckImages: true,
           maskIndicator: true,
-          enableManageAccounts: false,
+          enableManageAccounts: true,
         }},
     ]
 })
