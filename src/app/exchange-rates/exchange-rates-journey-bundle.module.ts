e5ba58802943d28de9dfc41b9bb5085c1822f1df
Mehrez Hrizi <mehrez.hrizi.official@gmail.com>
2022-04-27 00:16:01 +0100

    create Bundle for new Journey

---
+++ src/app/exchange-rates/exchange-rates-journey-bundle.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from "@angular/core";
+import { ExchangeRatesJourneyModule } from "exchange-rates-journey";
+
+@NgModule({
+    imports: [ExchangeRatesJourneyModule],
+    providers: []
+})
+export class ExchangeRatesJourneyBundleModule {}
