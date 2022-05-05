5304bc31becaf97d1c6e8c57ea337f8b3f19db28
Mehrez Hrizi <mehrez.hrizi.official@gmail.com>
2022-05-05 09:45:35 +0100

    connect journey service to the new API

--- projects/exchange-rates-journey/src/lib/exchange-rates-journey.service.ts
+++ projects/exchange-rates-journey/src/lib/exchange-rates-journey.service.ts
@@ -1,4 +1,9 @@
 import { Injectable, InjectionToken, Optional, Inject } from '@angular/core';
+import { HttpResponse } from '@angular/common/http';
+import { ExchangeHttpService, ExchangeRateItem } from 'exchange-rates-api';
+import { Observable, of, from as rxFrom} from 'rxjs';
+import { map, switchMap } from 'rxjs/operators';
+
 
 export const ExchangeRateConfigurationToken = new InjectionToken<ExchangeRateConfiguration>('ExchangeRateServiceConfig injection token');
 
@@ -10,6 +15,8 @@ export const DEFAULT_EXCHANGE_RATE_CONFIG: ExchangeRateConfiguration = {
   headerTitle: 'Welcome!',
 }
 
+export { ExchangeRateItem }
+
 @Injectable({
   providedIn: 'root'
 })
@@ -17,9 +24,18 @@ export class ExchangeRatesJourneyService {
 
   readonly serviceConfig: ExchangeRateConfiguration;
   constructor(
+    private readonly exchangeRateDataService: ExchangeHttpService,
     @Optional() @Inject(ExchangeRateConfigurationToken) serviceConfig: Partial<ExchangeRateConfiguration> | null
   ) {
     this.serviceConfig = {...DEFAULT_EXCHANGE_RATE_CONFIG, ...serviceConfig };
     console.log('Service configuration:', this.serviceConfig);
    }
+
+   getExchangeRates(source: string, target: string, from: string = '', to: string = '') { //<-- CREATE THIS METHOD
+    return this.exchangeRateDataService.getRates({ source, target, from, to }).pipe(
+      switchMap((response: ExchangeRateItem[]): Observable<ExchangeRateItem> => {
+        return response ? rxFrom(response) : of({} as ExchangeRateItem);
+      }),
+    )
+  }
 }
